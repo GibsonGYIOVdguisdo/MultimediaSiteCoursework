9f926f6c4f367430492b3502f52b33d8848ad29b
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-11-16 16:52:33 +0000

    Added unstyled set time button

--- clock/clock.js
+++ clock/clock.js
@@ -1,16 +1,43 @@
-const sec = document.getElementById("sec");
-const min = document.getElementById("min");
-const hour = document.getElementById("hour");
+const timeInput = document.getElementById("time-input");
+const timeButton = document.getElementById("set-time");
 
-function update() {
-  const now = new Date();
-  const s = now.getSeconds();
-  const m = now.getMinutes();
-  const h = now.getHours();
+const secondHand = document.getElementById("second-hand");
+const minuteHand = document.getElementById("minute-hand");
+const hourHand = document.getElementById("hour-hand");
 
-  sec.style.transform = `rotate(${s / 60}turn)`;
-  min.style.transform = `rotate(${m / 60}turn)`;
-  hour.style.transform = `rotate(${h / 60}turn)`;
+let startingTime = new Date().getTime();
+let whenWasTimeModified = new Date().getTime();
+
+timeButton.addEventListener("click", () => {
+  const splitValue = timeInput.value.split(":");
+  const hours = parseInt(splitValue[0]);
+  const minutes = parseInt(splitValue[1]);
+  const seconds = parseInt(splitValue[2]);
+  let newTime = new Date();
+  newTime.setMinutes(minutes);
+  newTime.setHours(hours);
+  newTime.setSeconds(seconds);
+  console.log(hours);
+  startingTime = newTime.getTime();
+
+  whenWasTimeModified = new Date().getTime();
+});
+
+function setClockTime(hours = 0, minutes = 0, seconds = 0) {
+  console.log(hours, minutes, seconds);
+  secondHand.style.transform = `rotate(${seconds / 60}turn)`;
+  minuteHand.style.transform = `rotate(${minutes / 60}turn)`;
+  hourHand.style.transform = `rotate(${hours / 12}turn)`;
 }
-update();
-setInterval(update, 1000);
+
+function updateClock() {
+  const timeOffset = new Date().getTime() - whenWasTimeModified;
+  const time = new Date(startingTime + timeOffset);
+  const seconds = time.getSeconds();
+  const minutes = time.getMinutes();
+  const hours = time.getHours();
+  setClockTime(hours, minutes, seconds);
+}
+
+updateClock();
+setInterval(updateClock, 1000);
