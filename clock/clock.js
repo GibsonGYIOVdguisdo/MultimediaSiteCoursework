e140a9f0b694a8acf6a994dcdd67d1b4d709678c
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2025-11-16 17:52:10 +0000

    Made second hand tick

--- clock/clock.js
+++ clock/clock.js
@@ -40,9 +40,19 @@ function saveCurrentTime() {
   localStorage.setItem("whenWasTimeModified", whenWasTimeModified);
 }
 
-function setClockTime(hours = 0, minutes = 0, seconds = 0) {
-  console.log(hours, minutes, seconds);
-  secondHand.style.transform = `rotate(${seconds / 60}turn)`;
+function setClockTime(hours = 0, minutes = 0, seconds = 0, milliseconds = 0) {
+  let tickMilliseconds = 100;
+
+  let secondHandRotation = seconds / 60;
+
+  if (milliseconds > 1000 - tickMilliseconds / 2) {
+    secondHandRotation =
+      secondHandRotation + 1 / 120 + ((1 / 60) * (milliseconds - 900)) / 100;
+  } else if (milliseconds > 1000 - tickMilliseconds) {
+    secondHandRotation =
+      secondHandRotation + ((1 / 60) * (milliseconds - 900)) / 200;
+  }
+  secondHand.style.transform = `rotate(${secondHandRotation}turn)`;
   minuteHand.style.transform = `rotate(${minutes / 60}turn)`;
   hourHand.style.transform = `rotate(${hours / 12}turn)`;
 }
@@ -50,11 +60,12 @@ function setClockTime(hours = 0, minutes = 0, seconds = 0) {
 function updateClock() {
   const timeOffset = new Date().getTime() - whenWasTimeModified;
   const time = new Date(startingTime + timeOffset);
+  const milliseconds = time.getMilliseconds();
   const seconds = time.getSeconds();
   const minutes = time.getMinutes();
   const hours = time.getHours();
-  setClockTime(hours, minutes, seconds);
+  setClockTime(hours, minutes, seconds, milliseconds);
 }
 
 updateClock();
-setInterval(updateClock, 1000);
+setInterval(updateClock, 1);
